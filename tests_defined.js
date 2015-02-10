9b6abfb7cf49df3f929d63a2609df1c5e5fca40b
Elias Moosman <elias@youiest.com>
2015-02-10 16:08:41 +0400

    starting sudo code for hooks react approach

--- tests_defined.js
+++ tests_defined.js
@@ -1,11 +1,1 @@
-Tinytest.add('peerdb - defined', function (test) {
-  var isDefined = false;
-  try {
-    Document;
-    isDefined = true;
-  }
-  catch (e) {
-  }
-  test.isTrue(isDefined, "Document is not defined");
-  test.isTrue(Package['peerlibrary:peerdb'].Document, "Package.peerlibrary:peerdb.Document is not defined");
-});
+
