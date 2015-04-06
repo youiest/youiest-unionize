c91ba8c41176f7b09bc0b860af98fa817f3492d1
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-04-06 13:51:46 +0530

    test

--- client.js
+++ client.js
@@ -16,13 +16,21 @@ Session.setDefault("inbox", []);
 Session.setDefault("outbox", []);
 Session.setDefault("follow", []);
 Session.setDefault("big", []);
+Unionize.onChangeFlag = false;
 Unionize.onChangeClient = function(doc){
+	if(Unionize.onChangeFlag)
+		return;
 	if(userId == doc._id){
-		Session.set("inbox",doc.inbox);
-		Session.set("outbox",doc.outbox);
-		Session.set("follow",doc.follow);
-		Session.set("big",doc.big);
+		if(doc.inbox && Session.get("inbox").length != doc.inbox.length)
+			Session.set("inbox",doc.inbox);
+		if(doc.outbox && Session.get("outbox").length != doc.outbox.length)
+			Session.set("outbox",doc.outbox);
+		if(doc.follow && Session.get("follow").length != doc.follow.length)
+			Session.set("follow",doc.follow);
+		if(doc.big && Session.get("big").length != doc.big.length)
+			Session.set("big",doc.big);
 	}
+	Unionize.onChangeFlag = false;
 }
 
 // WI.after.insert(function(userId, doc, fieldNames, modifier, options){
