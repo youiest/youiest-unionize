fc9e404bece9d98a70858cebfb6bedf818e3f428
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-04-10 12:29:55 +0530

    moving on both side

--- lib.js
+++ lib.js
@@ -45,3 +45,53 @@ Unionize.connect = function(docs){
 	docs.journey = [{"onConnect": Unionize.getUTC()- docs.startTime}];
 	WI.update(docs.from_user,{$push: {"outbox": docs}});
 }
+
+
+// hooks
+
+Unionize.onWUpdateHook = function(userId, docs){
+  // log("Unionize.onWInsertHook");
+  // log(docs)
+  docs.journey.push({"onWUpdateHook": Unionize.getUTC()- docs.startTime});
+
+  W.insert(docs);
+
+  docs.journey.push({"onInsertW": Unionize.getUTC()- docs.startTime});
+
+  Unionize.prepare(docs.to_user);
+  
+  WI.update(docs.to_user,{$push: {"inbox": docs}});
+  docs.journey.push({"onInsertWIInbox": Unionize.getUTC()- docs.startTime});
+  // if(WI.find(docs.to_user).count()){
+  //   // log("to_user updated");
+  // }
+  return docs;
+  
+  // replicated on W collection
+}
+
+// WI.insert.before(function(docs){
+  
+// });
+
+// WI.insert.after(function(docs){
+  
+// });
+
+// log(W.before.insert())
+W.before.insert(function(userId, docs){
+  // Unionize.onWInsertHook(userId, docs);
+});
+
+WI.before.update(function(userId, doc, fieldNames, modifier, options){
+  // log(fieldNames[0])
+  if(fieldNames[0] == "outbox"){
+    modifier["$push"].outbox = Unionize.onWUpdateHook(userId, modifier["$push"].outbox);
+    var docs = modifier["$push"].outbox
+    docs.journey.push({"onInsertWIInbox": Unionize.getUTC()- docs.startTime});
+    // log(userId, doc, fieldNames, modifier, options)    
+  }
+});
+// W.after.update(function(){
+  
+// });
