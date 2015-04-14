f1d5f10736a2d8641036f14cd9caa65e46987c9d
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-04-14 12:21:03 +0530

    more modularity

--- lib.js
+++ lib.js
@@ -32,14 +32,17 @@ Unionize.prepare = function(userId){
 	}
 	return true;
 }
+Unionize.validateDocs = function(docs){
+  if(!docs)
+    throw new new Meteor.Error("Please check information provided undefined", "404");
+  if(!docs.from_user){
+    throw new new Meteor.Error("Source is not defined from_user", "404");   
+  }
+  if(!docs.to_user)
+    throw new new Meteor.Error("Target is not defined to_user", "404");
+}
 Unionize.connect = function(docs){
-	if(!docs)
-		throw new new Meteor.Error("Please check information provided undefined", "404");
-	if(!docs.from_user){
-		throw new new Meteor.Error("Source is not defined from_user", "404");		
-	}
-	if(!docs.to_user)
-		throw new new Meteor.Error("Target is not defined to_user", "404");
+	Unionize.validateDocs(docs);
 	
 	Unionize.prepare(docs.from_user);
   
